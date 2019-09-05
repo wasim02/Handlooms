f3e6f3585e9085a83e0a1b24c0626a1d8864efe4
Wasim <wasimshaikh.7706@gmail.com>
2019-09-05 20:24:51 +0530

    Added my project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { SignInComponent } from './auth/sign-in/sign-in.component';
+import { RegistrationComponent } from './auth/registration/registration.component';
+
+const ROUTES: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'auth/signin', component: SignInComponent},
+  {path: 'auth/registration', component: RegistrationComponent},
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    SignInComponent,
+    RegistrationComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(ROUTES)
   ],
   providers: [],
   bootstrap: [AppComponent]
